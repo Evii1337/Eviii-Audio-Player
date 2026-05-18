be6b6c235fecfd5df542ebd282f6a4d9dd1590c4
EviZi <130212842+Evii1337@users.noreply.github.com>
2026-05-18 20:32:59 +0300

    Wrong folder

--- EviAudioPlayer/API/AudioClipPlayback.cs
+++
@@ -1,8 +0,0 @@
-namespace EviAudio.API;
-
-public static class AudioClipPlayback
-{
-    public const int PacketSize = 480;
-    public const int SamplingRate = 48000;
-    public const int Channels = 1;
-}
